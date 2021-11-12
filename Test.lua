b436bfa378c18ed404fbb72be00734e49b5a6524
ZoeyZhao-96 <zoeyzhao@lilith.com>
2021-11-12 20:37:56 +0800

    main

--- Test.lua
+++ Test.lua
@@ -1,16 +1,8 @@
 --- Fork Test
 
 local conflictTest = 1
-local a = 22
-local b = 33
-local c = a + b
-
-local maintest = 4
---local mt = 5
-local name = 888
-local l = 11231231
-e = 2222
-f = 44444
 I am Main Branch 
-I am New Change
- 1111
+I am Main Branch 
+I am Main Branch 
+I am Main Branch 
+I am Main Branch 
